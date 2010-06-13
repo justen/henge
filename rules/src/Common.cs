26beb2112adba62458c41182d1c2c45838c55520
Justen Hyde <Justen.Hyde@gmail.com>
2010-06-13 21:49:45 +0100

    Moved the clunky rules base stuff for dealing with energy into the Constants static class in Henge.Rules (and renamed it Common).

--- rules/src/Common.cs
+++ rules/src/Common.cs
@@ -2,23 +2,22 @@
 using System;
 using Henge.Data.Entities;
 
-
 namespace Henge.Rules
 {
 
 
-	public abstract class HengeProtagonist : ProtagonistRule
+	static class Common
 	{
-
-		public HengeProtagonist ()
-		{
-		}
-
-		//attempt to use the specified amount of energy of the actor. Return false if the actor didn't have enough energy.
+		//maximum skill level
+		public static long skillMax = 1000;
+		// seconds per fast tick
+		public static long fastTick = 5;
+		
+				//attempt to use the specified amount of energy of the actor. Return false if the actor didn't have enough energy.
 		//if overdraw = true, the actor can use more energy than it has BUT cannot use any energy if it is already negative
-		protected bool UseEnergy(Actor actor, long energy, bool overdraw)
+		public static bool UseEnergy(Actor actor, long energy, bool overdraw)
 		{
-			this.FastTick(actor);
+			Common.FastTick(actor);
 			if (overdraw || ( energy <= actor.Attributes["energy"] ))
 			{
 				if ( actor.Attributes["energy"]>0)
@@ -31,7 +30,7 @@ namespace Henge.Rules
 		}
 		
 		//charge up the actor with the specified amount of energy. Returns the amount of energy actually added to the actor
-		protected void FastTick(Actor actor)
+		public static void FastTick(Actor actor)
 		{
 			if (actor.Attributes.ContainsKey("maxEnergy")==false)
 			{
@@ -47,11 +46,11 @@ namespace Henge.Rules
 				if (actor.Attributes.ContainsKey("fitness") && (actor.Attributes["fitness"]>0))
 				{
 					TimeSpan ts = DateTime.Now - actor.LastModified;
-					long ticks = ((long)ts.TotalSeconds/Constants.fastTick);
+					long ticks = ((long)ts.TotalSeconds/Common.fastTick);
 					if (ticks>0)
 					{
-						actor.LastModified.AddSeconds(ticks * Constants.fastTick);
-						actor.Attributes["energy"] +=  ticks * (actor.Attributes["fitness"] /Constants.skillMax);
+						actor.LastModified.AddSeconds(ticks * Common.fastTick);
+						actor.Attributes["energy"] +=  ticks * (actor.Attributes["fitness"] /Common.skillMax);
 						if (actor.Attributes["energy"] > actor.Attributes["maxEnergy"]) actor.Attributes["energy"] = actor.Attributes["maxEnergy"];
 					}
 				}
@@ -59,9 +58,9 @@ namespace Henge.Rules
 		}
 		
 		//returns the current energy level of the actor
-		protected long GetEnergy (Actor actor)
+		public static long GetEnergy (Actor actor)
 		{
-			this.FastTick(actor);
+			Common.FastTick(actor);
 			return actor.Attributes["energy"];
 		}
 	}
--- rules/src/Move/Run/BasicRunProtagonist.cs
+++ rules/src/Move/Run/BasicRunProtagonist.cs
@@ -5,7 +5,7 @@ using Henge.Data.Entities;
 
 namespace Henge.Rules.Protagonist.Move.Run
 {
-	public class BasicRun : HengeProtagonist
+	public class BasicRun : ProtagonistRule
 	{
 		public BasicRun()
 		{
