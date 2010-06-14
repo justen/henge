828935a7cf2ccec516f0afe5652fd99b360c23a5
Justen Hyde <Justen.Hyde@gmail.com>
2010-06-14 21:15:30 +0100

    * Added a SkillCheck method in Henge.Rules.Common.cs which takes responsibility for carrying out skill checks and granting extra skill points * Added a Skill Entity which allows us to have child skills daisy-chained from skills

---
+++ data/src/Object/Entities/Skill.cs
@@ -0,0 +1,34 @@
+
+using System;
+using System.Collections.Generic;
+
+namespace Henge.Data.Entities
+{
+
+
+	public class Skill
+	{
+		public double Value 			{get; set;}
+		public IList<string> Children	{get; set;}
+		
+		public Skill()
+		{
+			this.Children = new List<string>();	
+		}
+		
+		public double Subtract (double quantity)
+		{
+			this.Value-=quantity;
+			if (this.Value<0.0) this.Value = 0.0;
+			return this.Value;
+		}	
+		
+		
+		public double Add (double quantity)
+		{
+			this.Value+=quantity;
+			if (this.Value>1.0) this.Value = 1.0;
+			return this.Value;			
+		}
+	}
+}
